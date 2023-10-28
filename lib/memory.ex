0febadf30d030103ea4439a9cba2de5deb0a319f
Avataw <avataw.dev@gmail.com>
2023-10-28 14:42:33 +0200

    add timer and tries to game

--- lib/memory.ex
+++ lib/memory.ex
@@ -6,4 +6,22 @@ defmodule Memory do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  defmodule Card do
+    @valid_types [:logo, :code]
+    defstruct id: "", language: "", type: nil
+
+    def new(language, type), do: %Card{id: Ecto.UUID.generate(), language: language, type: type}
+
+    def new_pair(language) do
+      [
+        new(language, :logo),
+        new(language, :code)
+      ]
+    end
+
+    def matches?({first_language, first_type}, {second_language, second_type}) do
+      first_language == second_language && first_type != second_type
+    end
+  end
 end
--- lib/memory_web/controllers/game_controller.ex
+++ lib/memory_web/controllers/game_controller.ex
@@ -2,6 +2,11 @@ defmodule MemoryWeb.GameController do
   use MemoryWeb, :controller
 
   def play(conn, _params) do
-    render(conn, :game)
+    cards =
+      ["python", "java", "c", "ruby", "javascript", "haskell", "go", "rust"]
+      |> Enum.flat_map(&Memory.Card.new_pair/1)
+      |> Enum.shuffle()
+
+    render(conn, :game, cards: cards)
   end
 end
